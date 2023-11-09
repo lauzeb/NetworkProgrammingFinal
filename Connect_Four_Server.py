053486715fe57ef7c1f461b1de773c26fd0d0172
RabihElmassih <46427963+RabihElmassih@users.noreply.github.com>
2023-11-09 16:58:42 -0500

    Update Connect_Four_Server.py
    v0.1

--- Connect_Four_Server.py
+++ Connect_Four_Server.py
@@ -7,6 +7,8 @@ Created on Fri Oct 27 15:10:40 2023
 
 
 import socket
+import threading
+import random
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -17,27 +19,55 @@ s.listen(30)
 
 print("Server side")
 
-while True:
-    
-   clientsocket, address = s.accept()
-   
-   while True:
-    
-       message = clientsocket.recv(2048).decode()
-    
-       print(f"Client: {message}")
-    
-       if (message == 'end'):
-            break
-    
-       inputs = input("Server: ")
-       clientsocket.send(inputs.encode())
-    
-       if (inputs == 'end'):
-            break
-               
-   print("This is the conclusion of the session.")
-    
-   clientsocket.close()
+activeGames = []
 
-s.close()
+def startGame(clientsocket):
+    password = random.randint(1000, 9999) #password random gen. If password is in use random gen again
+    newGame = {
+        'password': password,
+        'player1': clientsocket
+        }
+    activeGames.append(newGame)
+    clientsocket.sendall(str(password).encode())
+    print("Started new game with password", password)
+    
+
+def respond(clientsocket, address, clientData):
+    """
+    Supported commands [WIP]:
+        STARTGAME
+        JOINGAME [passwd]
+        EXITGAME
+    """
+   
+    commandData = clientData.split()
+    command = commandData[0]
+    
+    match command:
+        case "STARTGAME":
+           startGame(clientsocket)
+        case "JOINGAME":
+            pass
+            #call joingame method
+        
+    
+
+# Individual thread spawned for each connected client
+def clientThread(clientsocket, address):
+    while True:
+        message = clientsocket.recv(2048).decode()
+     
+        respond(clientsocket, address, message)
+                
+        
+# Server main loop
+while True:
+   clientsocket, address = s.accept()
+   print("New connection from client", address)
+   newThread = threading.Thread(target=clientThread, args=(clientsocket, address))
+   newThread.start()
+   
+
+
+    
+    
