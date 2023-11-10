e7fb73d9fe5723ed5bae29f83aa6f3aa40154b6f
RabihElmassih <46427963+RabihElmassih@users.noreply.github.com>
2023-11-10 17:28:56 -0500

    Update Connect_Four_Server.py

--- Connect_Four_Server.py
+++ Connect_Four_Server.py
@@ -10,63 +10,110 @@ import socket
 import threading
 import random
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class ConnectFourGameSession:
+    def __init__(self, password, player1):
+        self.password = password
+        self.players = [player1, None]
+        self.game_board = [[0] * 7 for _ in range(6)] #2d game board
+        
+    def add_player(self, player2_socket):
+        if self.players[1] is None:
+            self.players[1] = player2_socket
+            return True  
+        else:
+            return False  
 
-Port = 1234
-
-s.bind(('',Port))
-s.listen(30)
-
-print("Server side")
-
-activeGames = []
-
-def startGame(clientsocket):
-    password = random.randint(1000, 9999) #password random gen. If password is in use random gen again
-    newGame = {
-        'password': password,
-        'player1': clientsocket
-        }
-    activeGames.append(newGame)
-    clientsocket.sendall(str(password).encode())
-    print("Started new game with password", password)
+class ConnectFourServer:
     
-
-def respond(clientsocket, address, clientData):
-    """
-    Supported commands [WIP]:
-        STARTGAME
-        JOINGAME [passwd]
-        EXITGAME
-    """
-   
-    commandData = clientData.split()
-    command = commandData[0]
+    def __init__(self,port):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.port = port
+        self.s.bind(('', self.port))
+        self.s.listen(30)
+        self.activeGames = []
+        
+        # Server main loop
+    def start(self):
+        print("Server side")
+        while True:
+            clientsocket, address = self.s.accept()
+            print("New connection from client", address)
+            newThread = threading.Thread(target=self.clientThread, args=(clientsocket, address))
+            newThread.start()
     
-    match command:
-        case "STARTGAME":
-           startGame(clientsocket)
-        case "JOINGAME":
-            pass
-            #call joingame method
+        
+    def startGame(self, clientsocket):
+        password = random.randint(1000, 9999)
+        while self.passwordInUse(password):
+            password = random.randint(1000, 9999)
+
+        newGame = ConnectFourGameSession(password, clientsocket)
+        self.activeGames.append(newGame)
+        clientsocket.sendall(str(password).encode())
+        print("Started new game with password", password)
+        
+    def passwordInUse(self, password):
+        for game in self.activeGames:
+            if password == game.password:
+                return True
+        return False
+    
+    def passwordMatch(self,password): #called in JOIN GAME METHOD
+        for game in self.activeGames:
+            if(password == game.password):
+                #connect users and active game
+                return True # replace with ^
+            
+        raise Exception("invalid password") #GUI dialog box        
         
     
-
-# Individual thread spawned for each connected client
-def clientThread(clientsocket, address):
-    while True:
-        message = clientsocket.recv(2048).decode()
-     
-        respond(clientsocket, address, message)
-                
+    def respond(self,clientsocket, address, clientData):
+        """
+        Supported commands [WIP]:
+            PLAY
+            STARTGAME
+            JOINGAME [passwd]
+            EXITGAME
+            BACK
+            AGAIN
+            -PLAYABLE MOVES [0 - 6]
+        """
+       
+        commandData = clientData.split()
+        command = commandData[0]
         
-# Server main loop
-while True:
-   clientsocket, address = s.accept()
-   print("New connection from client", address)
-   newThread = threading.Thread(target=clientThread, args=(clientsocket, address))
-   newThread.start()
-   
+        match command:
+            case "PLAY":
+                pass
+                #call PLAY method
+            case "STARTGAME":
+               self.startGame(clientsocket)
+            case "JOINGAME":
+                pass
+                #call JOINGAME [password] method
+            case "EXITGAME":
+                pass
+                #call EXITGAME method
+            case "BACK":
+                pass
+                #call BACK method            
+            case "AGAIN":
+                pass
+                #call AGAIN method
+            case "MOVES":
+                pass
+                #call MOVES method
+    
+    # Individual thread spawned for each connected client
+    def clientThread(self, clientsocket, address):
+        while True:
+            message = clientsocket.recv(2048).decode()
+            
+            self.respond(clientsocket, address, message)
+                            
+if __name__ == "__main__":
+    server = ConnectFourServer(1234)
+    server.start()
 
 
     
