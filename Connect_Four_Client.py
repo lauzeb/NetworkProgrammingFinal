73f2def91bbd396c035ba71fe157831298320447
RabihElmassih <46427963+RabihElmassih@users.noreply.github.com>
2023-11-15 16:25:45 -0500

    Update Connect_Four_Client.py
    GUI v0.2
    added stubs for more buttons

--- Connect_Four_Client.py
+++ Connect_Four_Client.py
@@ -11,53 +11,74 @@ import sys
 
 Port = 1234
 Host ='127.0.0.1' #socket.gethostname()
-
+        
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s.connect((Host, Port))
 print("Client side")
-
+        
 #s.sendall("STARTGAME".encode())
 #print("Password from server: ", s.recv(2048).decode())
-
+        
 #secondpage = None
 
-class Ui(QtWidgets.QMainWindow):
+
+class FirstPage(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Ui, self).__init__()
+        super(FirstPage, self).__init__()
         uic.loadUi('title.ui', self)
         
         self.playButton = self.findChild(QtWidgets.QPushButton, "playButton")
-        self.playButton.clicked.connect(self.pressed)
+        self.playButton.clicked.connect(self.PressedPlay)
+
+        self.exitButton = self.findChild(QtWidgets.QPushButton, "exitButton")
+        self.exitButton.clicked.connect(self.PressedExit)
         
-        self.testLabel = self.findChild(QtWidgets.QLabel,"testLabel")
+        #self.testLabel = self.findChild(QtWidgets.QLabel,"testLabel")
         
         self.show()
         
-    def pressed(self):
-        print("Pressed play")
-        self.testLabel.setText("You pressed play!")
+    def PressedPlay(self):
         widget.setCurrentWidget(secondpage)
         
+    def PressedExit(self):
+        #closes GUI &
+        #send EXIT code to server
+        pass
         
-class SecondScreen(QtWidgets.QMainWindow):
+        
+class SecondPage(QtWidgets.QMainWindow):
     def __init__(self):
-        super(SecondScreen, self).__init__()
+        super(SecondPage, self).__init__()
         uic.loadUi('screen2.ui', self)
+    
+        self.createButton = self.findChild(QtWidgets.QPushButton, "createButton")        
+        self.createButton.clicked.connect(self.PressedCreate)
         
+        self.joinButton = self.findChild(QtWidgets.QPushButton, "joinButton")        
+        self.joinButton.clicked.connect(self.PressedJoin)
+
+        self.backButton = self.findChild(QtWidgets.QPushButton, "backButton")        
+        self.backButton.clicked.connect(self.PressedBack)        
         
         self.show()
+
+    def PressedJoin(self):
+        pass
+        # PULLS UP PASSWORD GUI
+        # ASKS FOR PASSWORD
+    
+    def PressedCreate(self):
+        pass
+        # SENDS STARTGAME 
         
-    def pressed(self):
-        print("Pressed play")
-        self.testLabel.setText("You pressed play!")
+    def PressedBack(self):
+        widget.setCurrentWidget(firstpage)
 
-
-       
-  
+        
 app = QtWidgets.QApplication(sys.argv)
 widget = QtWidgets.QStackedWidget()
-firstpage = Ui()
-secondpage = SecondScreen()
+firstpage = FirstPage()
+secondpage = SecondPage()
 widget.addWidget(firstpage)
 widget.addWidget(secondpage)
 widget.setCurrentWidget(firstpage)
@@ -65,5 +86,4 @@ widget.setCurrentWidget(firstpage)
 
 widget.show()
 app.exec_()
-#s.close()  
     
